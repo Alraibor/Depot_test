f25129d567ecc0aadcd23b07a17e4a1244c00390
Alraibor <alex.antoine@yahoo.fr>
2017-10-24 11:14:22 +0200

    ajout php
    retour.php

---
+++ retour.php
@@ -0,0 +1,25 @@
+<!DOCTYPE HTML>
+<html lang="fr">
+  <head>
+    <meta charset="UTF-8">
+		<title>Titre de la page HTML</title>
+		<meta name="robots" content="all"/>
+		<meta name="description" content="Modèle"/>
+		<meta name="author" content="DL2017-2018"/>
+<!--		<link href="styles.css" rel="stylesheet">
+				<script type="text/javascript" src="scripts.js"></script>-->
+		<style type="text/css">
+		/* Mes feuilles de style  */
+		* {margin:0; padding:0;}
+		
+		</style>
+		<script type="text/javascript">
+		/* Mes scripts  */
+		
+		</script>
+	</head>
+	<body>
+	<!-- Mes balises HTML  -->
+
+	</body>
+</html>
